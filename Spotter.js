ff880907d8ac72dc4126688e7ac16b29c2e1598e
crystal <chencrystal6@gmail.com>
2016-05-01 06:59:41 -0700

    connected mysql

--- Spotter.js
+++ Spotter.js
@@ -23,5 +23,38 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    var Connection = Tedious.Connection;
+
+var config = {
+    userName: 'mikewombat@lheu4c8fx8.database.windows.net',
+    password: 'Starexplorer1mic',
+    server: 'lheu4c8fx8.database.windows.net',
+    options: {
+        encrypt: true,
+        database: 'SpotterDatabase'
+    }
+};
+
+var connection = new Connection(config);
+
+connection.on('connect', function (err) {
+    var request = new Tedious.Request('SELECT TOP 20 * from dbo.SpotterTable', function (err, rowCount) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(rowCount + ' rows');
+        }
+    });
+
+    request.on('row', function (columns) {
+        var r = '';
+        columns.forEach(function (column) {
+            r = r + ' ' + column.value;
+        });
+        console.log('\n ', r);
+    });
+
+    connection.execSql(request);
+});
   });
 }
